f83a4024ca288f5cd5f519d269cb46b8c742bc6c
abubakar-octa <abubakar@octa.ai>
2020-04-29 10:59:35 +0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'demo-d3';
+export class AppComponent implements AfterViewInit {
+  constructor() {}
+
+  ngAfterViewInit() {
+    const canvas = d3.select('.canvas');
+
+    const svg = canvas.append('svg').attr('height', 600).attr('width', 600);
+
+    const group = svg.append('g').attr('transform', 'translate(0,100)');
+    // append shapes to svg container
+    group
+      .append('rect')
+      .attr('height', 100)
+      .attr('width', 200)
+      .attr('fill', 'blue')
+      .attr('x', 20)
+      .attr('y', 20);
+    group
+      .append('circle')
+      .attr('r', 50)
+      .attr('cx', 300)
+      .attr('cy', 70)
+      .attr('fill', 'pink');
+
+    group
+      .append('line')
+      .attr('x1', 370)
+      .attr('x2', 400)
+      .attr('y1', 20)
+      .attr('y2', 120)
+      .attr('stroke', 'red');
+
+    svg
+      .append('text')
+      .attr('x', 20)
+      .attr('y', 200)
+      .attr('fill', 'red')
+      .text('Hello D3')
+      .style('font-family', 'arial');
+  }
 }
