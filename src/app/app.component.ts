4f33da6043f44cfceb74e60da096468b1caae029
abubakar-octa <abubakar@octa.ai>
2020-05-06 11:51:00 +0500

    choropleth map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,91 +1,106 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import * as topojson from 'topojson';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements AfterViewInit, OnInit {
+  eduResp: any = [];
+  educations: any = [];
+  countiesResp: any = [];
+  counties: any = [];
+  data: any = {};
   constructor() {}
 
-  ngAfterViewInit() {
-    const menu = [
-      { name: 'soup', orders: 200 },
-      { name: 'curry', orders: 600 },
-      { name: 'pasta', orders: 300 },
-      { name: 'surprise meal', orders: 1000 },
-      { name: 'burger', orders: 1500 },
-    ];
+  async ngOnInit() {
+    this.eduResp = await fetch(
+      'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json'
+    );
+    this.educations = await this.eduResp.json();
+    this.countiesResp = await fetch(
+      'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
+    );
+    this.counties = await this.countiesResp.json();
+    this.data = await topojson.feature(
+      this.counties,
+      this.counties.objects.counties
+    );
+    debugger;
 
+    const path = d3.geoPath();
     const svg = d3
       .select('.canvas')
       .append('svg')
-      .attr('width', 600)
+      .attr('width', 1200)
       .attr('height', 600);
-
     // create margins and dimensions
     const margin = { top: 20, right: 20, bottom: 100, left: 100 };
     const graphWidth = 600 - margin.left - margin.right;
     const graphHeight = 600 - margin.top - margin.bottom;
 
-    const graph = svg
+    // Define the div for the tooltip
+    // var body = d3.select('body');
+    // var tooltip = body
+    //   .append('div')
+    //   .attr('class', 'tooltip')
+    //   .attr('id', 'tooltip')
+    //   .style('opacity', 0);
+    const tooltip = document.getElementById('tooltip');
+    svg
       .append('g')
-      .attr('width', graphWidth)
-      .attr('height', graphHeight)
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-
-    // axis groups
-    // to invert x-axis to the bottom
-    const xAxisGroup = graph
-      .append('g')
-      .attr('transform', `translate(0, ${graphHeight})`);
-    const yAxisGroup = graph.append('g');
-
-    // we use d3 scale to range our values in reasonable number of pixel height
-    // domain is our original data scale while range is what which will be scaled on the web page
-    const y = d3
-      .scaleLinear()
-      .domain([0, d3.max(menu, (m) => m.orders)])
-      .range([graphHeight, 0]);
-
-    // dynamically find min,max values from the array
-    const min = d3.min(menu, (m) => m.orders);
-    const max = d3.max(menu, (m) => m.orders);
-    // or we can use extent to get min and max in one go
-    // this will give us an array with min[0] and max[1]
-    const extent = d3.extent(menu, (m) => m.orders);
-
-    const x = d3
-      .scaleBand()
-      .domain(menu.map((m) => m.name))
-      .range([0, 500])
-      .paddingInner(0.2)
-      .paddingOuter(0.2);
-
-    const rects = graph.selectAll('rect').data(menu);
-
-    rects
-      .attr('width', x.bandwidth)
-      .attr('height', (menu) => graphHeight - y(menu.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name))
-      .attr('y', (d) => y(d.orders));
-
-    rects
+      .selectAll('path')
+      .attr('class', 'county')
+      .data(this.data.features)
       .enter()
-      .append('rect')
-      .attr('width', x.bandwidth)
-      .attr('height', (menu) => graphHeight - y(menu.orders))
-      .attr('fill', 'orange')
-      .attr('x', (d) => x(d.name))
-      .attr('y', (d) => y(d.orders));
+      .append('path')
+      .attr('fill', 'black')
+      .attr('d', path)
+      .on('mouseover', (d, i) => {
+        const { coordinates } = d.geometry;
+        const [x, y] = coordinates[0][0];
 
-    // create and call the axis
-    const xAxis = d3.axisBottom(x);
-    const yAxis = d3.axisLeft(y);
+        // matching and then mapping equal fips of baclor data with the svg graph
+        const education = this.educations.find((edu) => edu.fips === d.id);
 
-    xAxisGroup.call(xAxis);
-    yAxisGroup.call(yAxis);
+        tooltip.classList.add('show');
+        debugger;
+        tooltip.style.left = x - 50 + 'px';
+        tooltip.style.top = y - 50 + 'px';
+        tooltip.setAttribute('data-education', education.bachelorsOrHigher);
+
+        tooltip.innerHTML = `
+          <p>${education.area_name} - ${education.state}</p>
+          <p>${education.bachelorsOrHigher}</p>
+        `;
+      })
+      .on('mouseout', () => {
+        tooltip.classList.remove('show');
+      });
+    debugger;
+  }
+
+  ngAfterViewInit() {
+    // const path = d3.geoPath();
+    // const svg = d3
+    //   .select('.canvas')
+    //   .append('svg')
+    //   .attr('width', 600)
+    //   .attr('height', 600);
+    // // create margins and dimensions
+    // const margin = { top: 20, right: 20, bottom: 100, left: 100 };
+    // const graphWidth = 600 - margin.left - margin.right;
+    // const graphHeight = 600 - margin.top - margin.bottom;
+    // svg
+    //   .append('g')
+    //   .selectAll('path')
+    //   .data(this.data.features.slice(0, 10))
+    //   .enter()
+    //   .append('path')
+    //   .attr('fill', 'black')
+    //   .attr('d', path);
+    // debugger;
   }
 }
